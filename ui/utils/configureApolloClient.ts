869ed2022df6c6bb4951ccade48076247e8296b9
DeAndre Johnson <dre@phreshr.com>
2017-02-13 13:44:42 -0500

    update

--- ui/utils/configureApolloClient.ts
+++ ui/utils/configureApolloClient.ts
@@ -4,12 +4,21 @@ const networkInterface = createNetworkInterface({
   uri: process.env.GRAPHQL_ENDPOINT || 'https://api.graph.cool/simple/v1/cixm67lmh1yjd0177j5cwt47t'
 });
 
+networkInterface.use([{
+  applyMiddleware(req, next) {
+    if (!req.options.headers) {
+      req.options.headers = {};
+    }
+    next();
+  }
+}]);
+
 export default options => new ApolloClient(Object.assign({}, {
   networkInterface,
-  dataIdFromObject: (result) => {
-    if (result.id && result.__typename) {
-      return result.__typename + result.id;
-    }
-    return null;
-  }
+  // dataIdFromObject: (result) => {
+  //   if (result.id && result.__typename) {
+  //     return result.__typename + result.id;
+  //   }
+  //   return null;
+  // }
 }, options));
