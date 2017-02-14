23a981e2f3227d3dd9ba67e46eb00a8958e2877d
DeAndre Johnson <dre@phreshr.com>
2017-02-14 16:52:35 -0500

    use ssr caching

--- server/middleware/render.tsx
+++ server/middleware/render.tsx
@@ -5,16 +5,36 @@ import { renderToStaticMarkup } from 'react-dom/server';
 import { StaticRouter } from 'react-router-dom';
 import { ApolloProvider, renderToStringWithData } from 'react-apollo';
 import { styleSheet } from 'styled-components';
+import * as LRUCache from 'lru-cache';
 
 import App from 'ui/containers/App';
 import Html from 'server/views/Html';
 import configureApolloClient from 'ui/utils/configureApolloClient';
 import configureStore from 'ui/store/configureStore';
 
+// This is where we cache our rendered HTML pages
+const ssrCache = new LRUCache({
+  max: 100,
+  maxAge: 1000 * 60 * 60 // 1hour
+});
+
+function getCacheKey(req) {
+  return `${req.url}`;
+}
+
 export default (req, res) => {
   const context: any = {};
   const client = configureApolloClient({ ssrMode: true });
   const store = configureStore(client);
+
+  const key = getCacheKey(req);
+
+  // If we have a page in the cache, let's serve it
+  if (ssrCache.has(key)) {
+    console.log(`CACHE HIT: ${key}`);
+    res.send(ssrCache.get(key));
+    return;
+  }
 
   renderToStringWithData(
     <StaticRouter location={req.url} context={context}>
@@ -38,6 +58,10 @@ export default (req, res) => {
         styles={styles}
       />
     );
+
+    // Let's cache this page
+    console.log(`CACHE MISS: ${key}`);
+    ssrCache.set(key, `<!doctype html>${markup}`);
 
     return res
       .status(context.status || 200)
