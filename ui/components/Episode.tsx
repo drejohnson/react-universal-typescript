f38b7f78240e1ef54896d72ae5bb840dc9c4fc26
DeAndre Johnson <dre@phreshr.com>
2017-02-20 17:59:13 -0500

    added alt tag

--- ui/components/Episode.tsx
+++ ui/components/Episode.tsx
@@ -9,7 +9,7 @@ const Episode: React.SFC<Props> = ({ episode }) => (
     <h3>{episode.show.title}</h3>
       <h1>{episode.title}</h1>
       <p>{episode.description}</p>
-      <img src={episode.imageThumbUrl}/>
+      <img src={episode.imageThumbUrl} alt={episode.title}/>
       {episode.topics.map(topic =>
         <div key={topic.id}>{topic.name}</div>
       )}
