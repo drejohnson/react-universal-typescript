3eee296fac3357ccee2aec05e8b3c0308d09eeb6
DeAndre Johnson <dre@phreshr.com>
2017-02-23 17:26:11 -0500

    clean up

--- ui/components/EpisodeIndex.tsx
+++ ui/components/EpisodeIndex.tsx
@@ -1,35 +1,26 @@
 import * as React from 'react';
-import gql from 'graphql-tag';
 import { withApollo } from 'react-apollo';
-import { filter, propType } from 'graphql-anywhere';
 
 import configureApolloClient from 'ui/utils/configureApolloClient';
-import Episode from './Episode';
 
 import { AllEpisodesQuery } from 'ui/schema';
 
 const client = configureApolloClient({});
 const graphqlDocuments = require('ui/graphql/documents.json');
 
-const renderQuery = () => client.query<AllEpisodesQuery>({
-  query: graphqlDocuments['AllEpisodesQuery.graphql']
-}).then(({data}: {data: AllEpisodesQuery}) => {
-  data.allEpisodes.map(episode => `<Episode key={episode.id} episode={episode}/>`)
-});
-
 interface Props {}
 
 const EpisodeIndex: any = () => {
+  const renderQuery = () => client.query<AllEpisodesQuery>({
+    query: graphqlDocuments['AllEpisodesQuery.graphql']
+  }).then(({data}: {data: AllEpisodesQuery}) => {
+    data.allEpisodes.map(episode => console.log(episode));
+  });
   return (
     <div>
-      <h2>Episodes:</h2>
-      {renderQuery}
+      <h2>Episodes:</h2> 
     </div>
   );
 };
-
-// EpisodeIndex.fragments = {
-//   entry: gql``
-// };
 
 export default withApollo(EpisodeIndex);
