f5f9e6f10cbf70934d39c5969820f56832b02c3f
DeAndre Johnson <dre@phreshr.com>
2017-02-24 17:46:29 -0500

    testing conditional

--- ui/components/Episode.tsx
+++ ui/components/Episode.tsx
@@ -3,20 +3,35 @@ import { Link } from 'react-router-dom';
 
 interface Props {
   episode;
+  detail?: boolean;
 }
 
-const Episode: React.SFC<Props> = ({ episode }) => (
-  <article>
-    <Link to={`/hello/${episode.id}/${episode.uid}`}>
-      <h3>{episode.show.title}</h3>
-      <h1>{episode.title}</h1>
-      <p>{episode.description}</p>
-      <img src={episode.imageThumbUrl} alt={episode.title}/>
-      {episode.topics.map(topic =>
-        <div key={topic.id}>{topic.name}</div>
-      )}
-    </Link>
-  </article>
-);
+const Episode: React.SFC<Props> = ({ episode, detail }) => {
+  if (detail) {
+    return (
+      <article>
+        <h1>{episode.title}</h1>
+        <p>{episode.description}</p>
+        {episode.topics.map(topic =>
+          <div key={topic.id}>{topic.name}</div>
+        )}
+      </article>
+    );
+  } else {
+    return (
+    <article>
+      <Link to={`/episode/${episode.id}/${episode.uid}`}>
+        <h3>{episode.show.title}</h3>
+        <h1>{episode.title}</h1>
+        <p>{episode.description}</p>
+        <img src={episode.imageThumbUrl} alt={episode.title}/>
+        {episode.topics.map(topic =>
+          <div key={topic.id}>{topic.name}</div>
+        )}
+      </Link>
+    </article>
+  );
+  }
+};
 
 export default Episode;
