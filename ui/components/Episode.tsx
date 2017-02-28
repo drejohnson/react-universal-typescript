b0f453ae5462ad6c094216c18ab05286f5494823
DeAndre Johnson <dre@phreshr.com>
2017-02-28 15:36:38 -0500

    added video.js

--- ui/components/Episode.tsx
+++ ui/components/Episode.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import { Link } from 'react-router-dom';
+import VideoPlayer from './VideoPlayer';
 
 interface Props {
   episode;
@@ -8,10 +9,20 @@ interface Props {
 
 const Episode: React.SFC<Props> = ({ episode, detail }) => {
   if (detail) {
+    const videoJsOptions = {
+      autoplay: false,
+      controls: true,
+      aspectRatio: '16:9',
+      sources: [{
+        src: `${episode.videoUrl}`,
+        type: 'video/mp4'
+      }]
+    }
     return (
       <article>
         <h1>{episode.title}</h1>
         <p>{episode.description}</p>
+        <VideoPlayer {...videoJsOptions} />
         {episode.topics.map(topic =>
           <div key={topic.id}>{topic.name}</div>
         )}
---
+++ ui/components/VideoPlayer.tsx
@@ -0,0 +1,33 @@
+import * as React from 'react';
+import { compose, lifecycle, pure, toClass } from 'recompose';
+import * as videojs from 'video.js';
+
+export default class VideoPlayer extends React.Component<any, any> {
+  private player: any;
+  private videoNode: any;
+
+  public componentDidMount() {
+    // instantiate video.js
+    this.player = videojs(this.videoNode, this.props, function onPlayerReady() {
+      console.log('onPlayerReady', this);
+    });
+  }
+
+  // destroy player on unmount
+  public componentWillUnmount() {
+    if (this.player) {
+      this.player.dispose();
+    }
+  }
+
+  // wrap the player in a div with a `data-vjs-player` attribute
+  // so videojs won't create additional wrapper in the DOM
+  // see https://github.com/videojs/video.js/pull/3856
+  public render() {
+    return (
+      <div data-vjs-player>
+        <video ref={ node => this.videoNode = node } className="video-js"></video>
+      </div>
+    )
+  }
+}
